79eddfdeab131e93d6dbbdad111bfb0e0d69bfbb
Nathalia Vieira <83051644+nathsilvavieira@users.noreply.github.com>
2022-03-28 23:26:53 -0300

    Adc update e url_imagens

---
+++ update_imagens.sql
@@ -0,0 +1,78 @@
+-- Onix
+UPDATE imagens
+SET url_imagem = 'https://alucar-t1-g4.s3.amazonaws.com/modelos/chevrolet_onix-2020-2021.png'
+WHERE imagens_id = 1;
+-- 208
+UPDATE imagens
+SET url_imagem = 'https://alucar-t1-g4.s3.amazonaws.com/modelos/peugeot_208-2020-2021.png'
+WHERE imagens_id = 2;
+-- HB20
+UPDATE imagens
+SET url_imagem = 'https://alucar-t1-g4.s3.amazonaws.com/modelos/hyundai_hb20-2020-2021.png'
+WHERE imagens_id = 3;
+-- Sandero
+UPDATE imagens
+SET url_imagem = 'https://alucar-t1-g4.s3.amazonaws.com/modelos/renault_sandero.png'
+WHERE imagens_id = 4;
+-- Logan
+UPDATE imagens
+SET url_imagem = 'https://alucar-t1-g4.s3.amazonaws.com/modelos/renault-logan-2021.png'
+WHERE imagens_id = 5;
+-- Voyage
+UPDATE imagens
+SET url_imagem = 'https://alucar-t1-g4.s3.amazonaws.com/modelos/volkswagen_voyage-2020-2021.png'
+WHERE imagens_id = 6;
+-- Onix Plus
+UPDATE imagens
+SET url_imagem = 'https://alucar-t1-g4.s3.amazonaws.com/modelos/chevrolet_onix_plus-2020-2021.png'
+WHERE imagens_id = 7;
+-- Versa
+UPDATE imagens
+SET url_imagem = 'https://alucar-t1-g4.s3.amazonaws.com/modelos/nissan-versa-2022.png'
+WHERE imagens_id = 8;
+-- UP
+UPDATE imagens
+SET url_imagem = 'https://alucar-t1-g4.s3.amazonaws.com/modelos/volkswagen_up-2020-2021.png'
+WHERE imagens_id = 9;
+-- Mobi
+UPDATE imagens
+SET url_imagem = 'https://alucar-t1-g4.s3.amazonaws.com/modelos/fiat_mobi-2020-2021.png'
+WHERE imagens_id = 10;
+-- Gol
+UPDATE imagens
+SET url_imagem = 'https://alucar-t1-g4.s3.amazonaws.com/modelos/volkswagen_gol-2020-2021.png'
+WHERE imagens_id = 11;
+-- Kwid
+UPDATE imagens
+SET url_imagem = 'https://alucar-t1-g4.s3.amazonaws.com/modelos/renault_kwid-2020-2021.png'
+WHERE imagens_id = 12;
+-- 2008
+UPDATE imagens
+SET url_imagem = 'https://alucar-t1-g4.s3.amazonaws.com/modelos/peugeot-2008.png'
+WHERE imagens_id = 13;
+-- Duster
+UPDATE imagens
+SET url_imagem = 'https://alucar-t1-g4.s3.amazonaws.com/modelos/renault_duster-2020-2021.png'
+WHERE imagens_id = 14;
+-- Captur
+UPDATE imagens
+SET url_imagem = 'https://alucar-t1-g4.s3.amazonaws.com/modelos/renault-captur-2021.png'
+WHERE imagens_id = 15;
+-- T-Cross
+UPDATE imagens
+SET url_imagem = 'https://alucar-t1-g4.s3.amazonaws.com/modelos/vw_t-cross-2020-2021.png'
+WHERE imagens_id = 16;
+-- Compass
+UPDATE imagens
+SET url_imagem = 'https://alucar-t1-g4.s3.amazonaws.com/modelos/jeep-compass.jpg'
+WHERE imagens_id = 17;
+-- 3008
+UPDATE imagens
+SET url_imagem = 'https://alucar-t1-g4.s3.amazonaws.com/modelos/peugeot-3008-2021.png'
+WHERE imagens_id = 18;
+-- Tiguan
+UPDATE imagens
+SET url_imagem = 'https://alucar-t1-g4.s3.amazonaws.com/modelos/vw_tiguan.png'
+WHERE imagens_id = 19;
+
+SELECT * FROM imagens;
